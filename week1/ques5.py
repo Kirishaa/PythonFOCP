9ba4dcf99f13af7b98cf603c44ec43b3bfb44c3b
Krisha <shresthakrisha64@gmail.com>
2022-11-09 11:45:18 +0545

    Week 1 assignment.

---
+++ week1/ques5.py
@@ -0,0 +1,13 @@
+lab_group = 24
+first_grp = 113 // lab_group
+second_grp = 175 // lab_group
+third_grp = 12 // lab_group
+full_group = first_grp + second_grp + third_grp
+print ("There will be",full_group, "full groups.")
+f_left = 113 % 24
+s_left = 175 % 24
+t_left = 12 % 24
+left = f_left + s_left + t_left
+left_over = left // lab_group
+print ("There will be",left_over, "left group.")
+
